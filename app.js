2d114e679d568de4dc0975184f83c61ff223767d
unknown <you@example.comarq.cristianmasci@gmail.comgit config --global user.name Your>
2021-06-25 12:29:40 -0300

    first push

---
+++ app.js
@@ -0,0 +1,10 @@
+/*EFECTO MOVIMIENTO MENU*/ 
+const menu = document.getElementById("menu")
+window.addEventListener("scroll", () => {
+    if (window.scrollY > 100) {
+        menu.style.height="5vh"
+    } else {
+        menu.style.height="10vh"
+    }
+})
+/*EFECTO*/
