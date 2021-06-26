ca4146ee11ae44df0555a75317dc5a8a764724bf
unknown <you@example.comarq.cristianmasci@gmail.comgit config --global user.name Your>
2021-06-26 12:46:56 -0300

    burger animated

--- app.js
+++ app.js
@@ -10,12 +10,18 @@ window.addEventListener("scroll", () => {
 
 
 /*EFECTO MENU MOBILE*/
+const line1 = document.querySelector(`.line1`)
+const line2 = document.querySelector(`.line2`)
+const line3 = document.querySelector(`.line3`)
 const splashMenu = document.querySelector(`.splashMenu`)
 const burger = document.querySelector(`#burger`)
 burger.addEventListener("click",openMenu)
 
 function openMenu() {
     splashMenu.classList.toggle("active")
+    line1.classList.toggle("act1")
+    line2.classList.toggle("act2")
+    line3.classList.toggle("act3")
     console.log("probando")
 }
 
