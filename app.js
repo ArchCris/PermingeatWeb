c8e84d715be4ab2be42dc59705a7134c0a8fbd44
unknown <you@example.comarq.cristianmasci@gmail.comgit config --global user.name Your>
2021-06-27 11:15:36 -0300

    2

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
-/*EFECTO MOVIMIENTO MENU*/ 
+/*EFECTO MOVIMIENTO MENU*/
+let positionA = window.scrollY
 const menu = document.getElementById("menu")
 window.addEventListener("scroll", () => {
-    if (window.scrollY > 100) {
-        menu.style.height="5vh"
-    } else {
+    positionB = window.scrollY
+    if (positionA > positionB) {
         menu.style.height="10vh"
+    } else {
+        menu.style.height="5vh"
     }
+    positionA = positionB
 })
 
 /*EFECTO MENU MOBILE*/
@@ -25,8 +28,7 @@ function openMenu() {
 }
 
 /*PLANO CONTACTO*/
-
-var mymap = L.map('mapid').setView([-34.57592843737485, -58.43615979603104], 15);
+var mymap = L.map('mapid').setView([-34.57810926536335, -58.44511256218203], 13);
 
 L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
@@ -37,12 +39,12 @@ L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_toke
     accessToken: 'pk.eyJ1Ijoic2lyY3Jpc3MiLCJhIjoiY2tteXp1Z3ZxMDU1YzJvbDJndjJxMjR2aCJ9.LXdD0PNDsGWo2iwHuZnMAA'
 }).addTo(mymap);
 
-var marker = L.marker([-34.57592843737485, -58.43615979603104]).addTo(mymap);
+var marker = L.marker([-34.57810926536335, -58.44511256218203]).addTo(mymap);
 
 marker.addEventListener("click", redirection)
 
 function redirection() {
     console.log("fd")
-    window.location.replace("https://www.google.com.ar/maps/place/Melisa+Permingeat/@-34.5760173,-58.4373328,17z/data=!3m1!4b1!4m5!3m4!1s0x95bcb5942fde75e5:0xb05fd9ad9548e750!8m2!3d-34.5760242!4d-58.435147");
+    window.location.replace("https://www.google.com/maps/place/Tte.+Benjam%C3%ADn+Matienzo+3055,+C1426+DBE,+Buenos+Aires/@-34.578064,-58.4457308,19z/data=!3m1!4b1!4m5!3m4!1s0x95bcb5ea2dff961f:0x4a5a543d46b364b0!8m2!3d-34.5780651!4d-58.4451823");
 }
 
